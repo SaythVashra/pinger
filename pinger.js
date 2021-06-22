7a42d9a3bfeba4a35dfe3bbbc13fe941655b2c95
Sayth Vashra <marcosvs1248@gmail.com>
2021-06-22 15:00:51 -0400

    all working except for audio notifications, added date/time console log

--- pinger.js
+++ pinger.js
@@ -1,32 +1,58 @@
 const express = require("express");
 const app = express();
 const https = require("https");
-
-const connectAlarm = new Audio(__dirname + "/sounds/connect-alarm.mp3");
-const disconnectAlarm = new Audio(__dirname + "/sounds/disconnect-alarm.mp3");
+const play = require('audio-play');
+const load = require('audio-loader');
 
 let weAreOnline = false;
+let html = "";
+let appleCaptiveHtml = "<HTML><HEAD><TITLE>Success</TITLE></HEAD><BODY>Success</BODY></HTML>\n"
+let url = 'https://captive.apple.com/';
 
-function checkInternetConnection(){
+function getDate() {
 
-https.get('https://captive.apple.com/', (res) => {
-    console.log('statusCode:' + res.statusCode);
-    if (res.statusCode === 200) {
-        if (!weAreOnline) {
-            connectAlarm.play();
-            weAreOnline = true;
-            console.error("Internet connection found!!");
-        }
-    }
-}).on('error', (e) => {
-    if (weAreOnline) {
-        disconnectAlarm.play()
-        weAreOnline = false;
-        console.error("Internet connection lost...");
-    }
-});
+    const time = new Date();
 
+    const options = {
+        month: "numeric",
+        day: "numeric",
+        hour: "numeric",
+        minute: "numeric",
+        second: "numeric",
+    };
+
+    return time.toLocaleDateString("en-US", options);
 }
 
+function connectAlarm() {
+    load(__dirname + '/sounds/connect-alarm.mp3').then(play);
+}
 
-setInterval(checkInternetConnection, 10000);
+function disconnectAlarm() {
+    load('sounds/disconnect-alarm.mp3').then(play);
+}
+
+function checkInternetConnection() {
+
+    https.get(url, function (res) {
+
+        res.on("data", function (data) {
+
+            html = data.toString();
+
+            if (res.statusCode === 200 && html === appleCaptiveHtml) {
+                if (!weAreOnline) {
+                    weAreOnline = true;
+                    console.error("Internet connection found!!  " + getDate());
+                }
+            }
+        });
+    }).on('error', function (e) {
+        if (weAreOnline) {
+            weAreOnline = false;
+            console.error("Internet connection lost...  " + getDate());
+        }
+    });
+}
+
+setInterval(checkInternetConnection, 1000);
